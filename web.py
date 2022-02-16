e990ea03f1fa65b40a7072903c6a3d1f15ee3e4a
ahanC22 <98201205+ahanC22@users.noreply.github.com>
2022-02-16 18:40:00 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,4 @@
+import webbrowser
+f = open('/Users/ahan/Documents/Ahan/Coding/Design/web_home.html', 'r')
+filename = 'file:///'+'/Users/ahan/Documents/Ahan/Coding/Design/web_home.html'
+webbrowser.open_new_tab(filename)

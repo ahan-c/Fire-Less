48aebd3f0473c98b0b7f351b65c20fbc26680cce
ahanC22 <98201205+ahanC22@users.noreply.github.com>
2022-02-17 11:47:41 +0530

    Delete web.py

--- web.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-f = open('/Users/ahan/Documents/Ahan/Coding/Design/web_home.html', 'r')
-filename = 'file:///'+'/Users/ahan/Documents/Ahan/Coding/Design/web_home.html'
-webbrowser.open_new_tab(filename)
